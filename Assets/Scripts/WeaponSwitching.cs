c8f97afb2e27278bad2ffccf70ba7bddc59252ec
lyp3238 <lyp3238@gmail.com>
2020-10-24 01:43:11 +0900

    무기 스위칭 예제 제작

---
+++ Assets/Scripts/WeaponSwitching.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitching : MonoBehaviour
+{
+    public Animator anim;
+
+    public GameObject shield;
+    public GameObject sword;
+
+    public GameObject greatSword;
+
+    public bool isGreatSword = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGreatSword)
+        {
+            shield.SetActive(false);
+            sword.SetActive(false);
+
+            greatSword.SetActive(true);
+
+            anim.SetBool("IsGreatSword", isGreatSword);
+        }
+        else
+        {
+            shield.SetActive(true);
+            sword.SetActive(true);
+
+            greatSword.SetActive(false);
+
+            anim.SetBool("IsGreatSword", isGreatSword);
+        }
+    }
+}
